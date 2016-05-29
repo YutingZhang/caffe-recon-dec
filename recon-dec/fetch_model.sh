db3807920044f1436f5110f20f3d698b10a2cf4a
yutingzh <yutingzh@umich.edu>
2016-05-29 21:09:42 +0800

    script for fetch trained models

---
+++ recon-dec/fetch_model.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_PATH="$0"
+SCRIPT_DIR=`dirname "$0"`
+CUR_DIR=`pwd`
+cd "$SCRIPT_DIR"
+SCRIPT_DIR=`pwd -P`
+cd "$CUR_DIR"
+
+MODEL_PATH=$1
+
+if [ "$MODEL_PATH" == "" ]; then
+    cd "$SCRIPT_DIR"
+    find . -name '*_deploy.prototxt' |  while read line; do
+        MODEL_SUB_PATH=`echo "$line" | sed -e 's/_deploy\.prototxt$//'`
+        cd "$CUR_DIR"
+        "$0" "$MODEL_SUB_PATH"
+    done
+    exit
+fi
+
+URL_PREFIX="http://www.ytzhang.net/files/publications/2016-icml-recon-dec/models/"
+
+SUB_FOLDER=`dirname "$MODEL_PATH"`
+cd $SCRIPT_DIR
+if [ "$SUB_FOLDER" != "" ]; then
+    mkdir -p $SUB_FOLDER
+    cd "$SUB_FOLDER"
+fi
+
+wget -c "$URL_PREFIX""$MODEL_PATH".caffemodel
+
+
