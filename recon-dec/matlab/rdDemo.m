d5c250efe649eeaecbdc488dea3f16aa47c2495b
yutingzh <yutingzh@umich.edu>
2016-05-31 16:46:11 +0800

    reconstruction demo

--- recon-dec/matlab/rdDemo.m
+++ recon-dec/matlab/rdDemo.m
@@ -13,25 +13,31 @@ input_img_path = fullfile( RDMAT_ROOT, 'ILSVRC2012_val_00000011.JPEG' );
 I = imread(input_img_path);
 
 % reconstruction
-sae_first   = rdNet('vggnet/recon/SAE-first/layer5');
-swwae_first = rdNet('vggnet/recon/SWWAE-first/layer5');
-R_sae_first   = rdRun(sae_first,I);
-R_swwae_first = rdRun(swwae_first,I);
 
-figure;
-clf
-subplot(1,3,1);
-imshow( I );
-title( 'input' );
-subplot(1,3,2);
-imshow( R_sae_first );
-title( 'SAE-first (5th layer)' );
-subplot(1,3,3);
-imshow( R_swwae_first );
-title( 'SWWAE-first (5th layer)' );
+base_networks = {'alexnet','vggnet'};
 
+for b = 1:2
+
+    sae_first   = rdNet([base_networks{b} '/recon/SAE-first/layer5']);
+    swwae_first = rdNet([base_networks{b} '/recon/SWWAE-first/layer5']);
+    R_sae_first   = rdRun(sae_first,I);
+    R_swwae_first = rdRun(swwae_first,I);
+
+    figure;
+    clf
+    subplot(1,3,1);
+    imshow( I );
+    title( 'input' );
+    subplot(1,3,2);
+    imshow( R_sae_first );
+    title( sprintf( '%s-based\nSAE-first\n(5th layer)', base_networks{b}) );
+    subplot(1,3,3);
+    imshow( R_swwae_first );
+    title( sprintf( '%s-based\nSAE-first\n(5th layer)', base_networks{b}) );
+
+end
 
 % cls + reconstruction
-swwae_all   = rdNet('vggnet/cls/SWWAE-all/layer5');
-sae_all     = rdNet('vggnet/cls/SAE-all/layer5');
+%swwae_all   = rdNet('vggnet/cls/SWWAE-all/layer5');
+%sae_all     = rdNet('vggnet/cls/SAE-all/layer5');
 
